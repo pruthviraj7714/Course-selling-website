e3ba84a9517aa7125c86106f5d36ef44c5f602f9
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-06-29 14:57:18 +0530

    added loading page

--- src/app/courses/loading.tsx
+++ src/app/courses/loading.tsx
@@ -1,8 +1,10 @@
-
 export default function Loading() {
-    return(
-        <div className="flex justify-center items-center h-screen bg-purple-300">
-            Loading...
+    return (
+        <div className="flex justify-center items-center h-screen bg-black text-white">
+            <div className="flex flex-col items-center">
+                <div className="animate-spin rounded-full h-24 w-24 border-t-4 border-b-4 border-blue-500 mb-4"></div>
+                <div className="text-5xl">Loading...</div>
+            </div>
         </div>
     )
 }
