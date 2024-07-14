69e0a807ae04d1057f521838bb67aeb597fe682b
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-07-14 13:45:07 +0530

    format fix

--- src/actions/generateRandomTransactionId.ts
+++ src/actions/generateRandomTransactionId.ts
@@ -1,3 +1,8 @@
-export default function generateRandomTransactionId() {
-    return 'tx-' + Date.now().toString(36) + '-' + Math.random().toString(36).substr(2, 9);
+export default function generateRandomTransactionId(): string {
+  return (
+    "tx-" +
+    Date.now().toString(36) +
+    "-" +
+    Math.random().toString(36).substr(2, 9)
+  );
 }
